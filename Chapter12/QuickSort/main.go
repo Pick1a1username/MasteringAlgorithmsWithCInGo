842e2a3d20e73dcd452a116be28edba9e3e77258
Pick1a1username <xscreensaver@outlook.com>
2023-03-03 22:11:14 +0900

    20230303_0

--- Chapter12/QuickSort/main.go
+++ Chapter12/QuickSort/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
+	"sort"
+
 	"golang.org/x/exp/rand"
 )
-
-func main() {
-
-}
 
 // Compare two integers (used during median-of-three partitioning).
 func compareInt(int1, int2 *int) int {
@@ -19,15 +17,45 @@ func compareInt(int1, int2 *int) int {
 	}
 }
 
-func partition(data *[]int, i int, k int, compare func(key1, key2 int) int) {
-
-	// Allocate storage for the partition value and swapping.
-	pval := make([]int, len(*data))
-	temp := make([]int, len(*data))
-
+func partition(data *[]int, i int, k int, compare func(key1, key2 *int) int) int {
 	// Use the median-of-three method to find the partition value.
 	r := make([]int, 3)
 	r[0] = (rand.Int() % (k - i + 1)) + i
 	r[1] = (rand.Int() % (k - i + 1)) + i
 	r[2] = (rand.Int() % (k - i + 1)) + i
+	sort.Slice(r, func(i, j int) bool { return r[i] < r[j] })
+	pval := (*data)[r[1]]
+
+	// Create two partitions around the partition value.
+	for {
+		// Move left until an element is found in the wrong partition.
+		for compare(&(*data)[k], &pval) > 0 {
+			k--
+		}
+		// Move right until an element is found in the wrong partition.
+		for compare(&(*data)[i], &pval) < 0 {
+			i++
+		}
+
+		if i >= k {
+			// Stop partitioning when the left and right counters cross.
+			break
+		} else {
+			// Swap the elements now under the left and right counters.
+			(*data)[i], (*data)[k] = (*data)[k], (*data)[i]
+		}
+	}
+	return k
+}
+
+func qkSort(data *[]int, i int, k int, compare func(key1, key2 *int) int) int {
+	// Stop the recursion when it is not possible to partition further.
+	for i < k {
+		// Determine where to partition the elements.
+		j := partition(data, i, k, compare)
+		// Recursively sort the left partition.
+		qkSort(data, i, j, compare)
+		i = j + 1
+	}
+	return 0
 }
---
+++ Chapter12/QuickSort/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_test(t *testing.T) {
+	testCases := []struct {
+		description string
+		arr         []int
+		answer      []int
+	}{
+		{
+			description: "TestCase 1",
+			arr:         []int{24, 52, 11, 94, 28, 36, 14, 80},
+			answer:      []int{11, 14, 24, 28, 36, 52, 80, 94},
+		},
+		{
+			description: "TestCase 2",
+			arr:         []int{10, 5, 3, 6, 4, 9, 2, 1, 8, 7},
+			answer:      []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			result := append([]int{}, testCase.arr...)
+			qkSort(&result, 0, len(result)-1, compareInt)
+			if !reflect.DeepEqual(result, testCase.answer) {
+				t.Errorf("result: %d answer: %d", result, testCase.answer)
+			}
+		})
+	}
+}
