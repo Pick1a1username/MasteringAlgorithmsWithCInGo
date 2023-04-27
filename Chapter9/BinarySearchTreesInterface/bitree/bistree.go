a66501280b56dcc2972570b0533e133c976dfa6f
Pick1a1username <xscreensaver@outlook.com>
2023-04-27 22:23:50 +0900

    20230427_0

--- Chapter9/BinarySearchTreesInterface/bitree/bistree.go
+++ Chapter9/BinarySearchTreesInterface/bitree/bistree.go
@@ -146,7 +146,7 @@ func destroyRight(tree *BisTree, node *BiTreeNode) {
 
 func insert(tree *BisTree, node **BiTreeNode, data interface{}, balanced *int) int {
 	// Insert the data into the tree.
-	if *node != nil {
+	if *node == nil {
 		// Handle insertion into an empty tree.
 		avlData := AvlNode{
 			Factor: AvlBalanced,
@@ -156,7 +156,159 @@ func insert(tree *BisTree, node **BiTreeNode, data interface{}, balanced *int) i
 
 		return BiTreeInsLeft(tree, *node, avlData)
 	} else {
-		// Todo
+		// Handle insertion into a tree that is not empty.
+		cmpVal := tree.Compare(data, (*node).Data)
+
+		if cmpVal < 0 {
+			// Move to the left.
+			if (*node).Left == nil {
+				avlData := AvlNode{
+					Factor: AvlBalanced,
+					Hidden: 0,
+					Data:   data,
+				}
+				if BiTreeInsLeft(tree, *node, avlData) != 0 {
+					return -1
+				}
+				*balanced = 0
+			} else {
+				if retVal := insert(tree, &(*node).Left, data, balanced); retVal != 0 {
+					return retVal
+				}
+			}
+			// Ensure that the tree remains balanced.
+			if *balanced == 0 {
+				switch (*node).Data.(*AvlNode).Factor {
+				case AvlLftHeavy:
+					rotateLeft(node)
+					*balanced = 1
+				case AvlBalanced:
+					(*node).Data.(*AvlNode).Factor = AvlLftHeavy
+				case AvlRgtHeavy:
+					(*node).Data.(*AvlNode).Factor = AvlBalanced
+					*balanced = 1
+				default:
+					panic(fmt.Sprintf("unexpected Factor: %d", (*node).Data.(*AvlNode).Factor))
+				}
+			}
+		} else if cmpVal > 0 {
+			// Move to the right.
+			if (*node).Right == nil {
+				avlData := AvlNode{
+					Factor: AvlBalanced,
+					Hidden: 0,
+					Data:   data,
+				}
+				if BiTreeInsRight(tree, *node, avlData) != 0 {
+					return -1
+				}
+				*balanced = 0
+			} else {
+				if retVal := insert(tree, &(*node).Right, data, balanced); retVal != 0 {
+					return retVal
+				}
+			}
+			// Ensure that the tree remains balanced.
+			if *balanced == 0 {
+				switch (*node).Data.(*AvlNode).Factor {
+				case AvlLftHeavy:
+					(*node).Data.(*AvlNode).Factor = AvlBalanced
+					*balanced = 1
+				case AvlBalanced:
+					(*node).Data.(*AvlNode).Factor = AvlLftHeavy
+				case AvlRgtHeavy:
+					rotateRight(node)
+					*balanced = 1
+				default:
+					panic(fmt.Sprintf("unexpected Factor: %d", (*node).Data.(*AvlNode).Factor))
+				}
+			}
+		} else {
+			// Handle finding a copy of the data.
+			if (*node).Data.(*AvlNode).Hidden == 0 {
+				// Do nothing since the data is in the tree and not hidden.
+				return -1
+			} else {
+				// Insert the new data and mark it as not hidden.
+				if tree.Destroy != nil {
+					// Destroy the hidden data since it is being replaced.
+					tree.Destroy((*node).Data.(*AvlNode))
+				}
+				(*node).Data.(*AvlNode).Data = data
+				(*node).Data.(*AvlNode).Hidden = 0
+				// Do not rebalance because the tree structure is unchanged.
+				*balanced = -1
+			}
+		}
 	}
+	return 0
+}
+
+func hide(tree *BisTree, node *BiTreeNode, data interface{}) int {
+	if node == nil {
+		// Return that the data was not found.
+		return -1
+	}
+	cmpVal := tree.Compare(data, node.Data.(*AvlNode))
+	if cmpVal < 0 {
+		// Move to the left.
+		return hide(tree, node.Left, data)
+	} else if cmpVal > 0 {
+		// Move to the right.
+		return hide(tree, node.Right, data)
+	} else {
+		// Mark the node as hidden.
+		node.Data.(*AvlNode).Hidden = 1
+		return 0
+	}
+}
+
+func lookup(tree *BisTree, node *BiTreeNode, data *interface{}) int {
+	if node == nil {
+		// Return that the data was not found.
+		return -1
+	}
+	cmpVal := tree.Compare(*data, node.Data.(*AvlNode))
+	if cmpVal < 0 {
+		// Move to the left.
+		return lookup(tree, node.Left, data)
+	} else if cmpVal > 0 {
+		// Move to the right.
+		return lookup(tree, node.Right, data)
+	} else {
+		if node.Data.(*AvlNode).Hidden == 0 {
+			// Pass back the data from the tree.
+			// Is it ok?
+			*data = node.Data.(*AvlNode).Data
+			return 0
+		} else {
+			// Return that the data was not found.
+			return -1
+		}
+	}
+}
+
+func BisTreeInit(tree *BisTree, compare *CompareFunc, destroy *DestroyFunc) {
+	// Initialize the tree.
+	BiTreeInit(tree, *destroy)
+	tree.Compare = compare
+}
+
+func BisTreeDestroy(tree *BisTree) {
+	// Todo
+}
+
+func BisTreeInsert(tree *BisTree, data interface{}) int {
+	// Todo
+	return 0
+}
+
+func BisTreeRemove(tree *BisTree, data interface{}) int {
+	// Todo
+	return 0
+}
+
+func BisTreeLookup(tree *BisTree, data *interface{}) int {
+	// Todo
 	return 0
 }
